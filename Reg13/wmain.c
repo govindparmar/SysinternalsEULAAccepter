79a9bc1e954f1d43c05ec0add4c5fb8c6cda5e66
Govind Parmar <govind_parmar@cpr.ca>
2023-06-22 17:59:50 -0600

    Removed magic numbers and strings

--- Reg13/wmain.c
+++ Reg13/wmain.c
@@ -5,16 +5,22 @@
 
 #pragma comment(lib, "pathcch.lib")
 #define WMain wmain
+#define SYSINTERNALS_FOLDER L"C:\\SysInternals"
+#define SUBKEY_PATH L"SOFTWARE\\SysInternals"
+#define VALUE_NAME L"EulaAccepted"
+#define ACCEPTED_EULA 0x00000001
 
+_Check_return_
 _Success_(return == ERROR_SUCCESS)
+
 
 LSTATUS WINAPI InsertRegistryEntry(_In_reads_or_z_(MAX_PATH) WCHAR *wszFileName)
 {
-	LSTATUS lStatus = 0L;
+	LSTATUS lStatus;
 	HKEY hKey = NULL;
-	DWORD dwData = 0x00000001;
+	DWORD dwData = ACCEPTED_EULA;
 
-	lStatus = RegCreateKeyExW(HKEY_CURRENT_USER, L"SOFTWARE\\SysInternals", 0, NULL, 0, KEY_WRITE | KEY_READ, NULL, &hKey, NULL);
+	lStatus = RegCreateKeyExW(HKEY_CURRENT_USER, SUBKEY_PATH, 0, NULL, 0, KEY_WRITE | KEY_READ, NULL, &hKey, NULL);
 	if (lStatus != ERROR_SUCCESS)
 	{
 		goto cleanup;
@@ -26,11 +32,13 @@ LSTATUS WINAPI InsertRegistryEntry(_In_reads_or_z_(MAX_PATH) WCHAR *wszFileName)
 		goto cleanup;
 	}
 
-	lStatus = RegSetValueExW(hKey, L"EulaAccepted", 0, REG_DWORD, (LPBYTE)&dwData, sizeof(DWORD));
+	lStatus = RegSetValueExW(hKey, VALUE_NAME, 0, REG_DWORD, (LPBYTE)&dwData, sizeof(DWORD));
 	if (lStatus != ERROR_SUCCESS)
 	{
 		goto cleanup;
 	}
+
+	lStatus = ERROR_SUCCESS;
 
 cleanup:
 	if (hKey != NULL)
@@ -49,8 +57,7 @@ INT WINAPIV WMain (_In_ INT nArgc, _In_reads_(nArgc) WCHAR *pArgv[])
 	HANDLE hFind;
 	DWORD dwError = ERROR_SUCCESS;
 
-	// TODO: Replace with YOUR path to extracted Sysinternals folder
-	PathCchCombine(wszFullPath, MAX_PATH, L"C:\\SysInternals", L"*");
+	PathCchCombine(wszFullPath, MAX_PATH, SYSINTERNALS_FOLDER, L"*.exe");
 	hFind = FindFirstFileW(wszFullPath, &wfd);
 	if (INVALID_HANDLE_VALUE == hFind || NULL == hFind)
 	{
@@ -62,29 +69,24 @@ INT WINAPIV WMain (_In_ INT nArgc, _In_reads_(nArgc) WCHAR *pArgv[])
 	{
 		WCHAR *pExt = NULL, *pDot = NULL;
 		WCHAR wszFileArgument[MAX_PATH];
+		LSTATUS ls;
 
 		if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 		{
 			continue;
 		}
 		
-		PathCchFindExtension(wfd.cFileName, MAX_PATH, &pExt);
+		StringCchCopyW(wszFileArgument, MAX_PATH, wfd.cFileName);
 		
-		if (*pExt && (_wcsicmp(pExt, L"exe") == 0 || _wcsicmp(pExt, L".exe") == 0))
+		if ((ls = InsertRegistryEntry(wszFileArgument)) != ERROR_SUCCESS)
 		{
-			StringCchCopyW(wszFileArgument, MAX_PATH, wfd.cFileName);
-			if ((pDot = wcschr(wszFileArgument, L'.')) != NULL)
-			{
-				LSTATUS ls;
-
-				*pDot = L'\0';
-
-				if ((ls = InsertRegistryEntry(wszFileArgument)) != ERROR_SUCCESS)
-				{
-					fwprintf_s(stderr, L"Failed to create registry entry for %s: %I32u\n", wszFileArgument, ls);
-				}
-			}
+			fwprintf_s(stderr, L"Failed to create registry entry for %s: %I32u\n", wszFileArgument, ls);
 		}
+		else
+		{
+			wprintf_s(L"Successfully wrote registry entry for %s.\n", wszFileArgument);
+		}
+		
 	}
 	while (FindNextFileW(hFind, &wfd));
 	FindClose(hFind);
